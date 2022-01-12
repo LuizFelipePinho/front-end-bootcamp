19b864b4a4090550d7c538e07f9e94fa83092e27
anacgsantana <anacgsantana@gmail.com>
2022-01-11 21:53:46 -0300

    Up Contact and About

--- src/Components/Shared/SecHeader.js
+++ src/Components/Shared/SecHeader.js
@@ -1,38 +1,20 @@
+import React, { useState, useEffect } from "react";
 import "./SecHeader.css";
+
 import { Link } from "react-router-dom";
-import logo from "./img/logo.png";
+import logo from "./img/logo1.png";
 
-export default function Navbar() {
-  return (
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container-fluid">
-        
-        <Link to="/">
-          <a class="navbar-brand" href="#">
-            <img src={logo} />
-          </a>
-        </Link>
-        <form class="d-flex">
-          <select
-            class="form-select form-select-sm"
-            aria-label=".form-select-sm example">
-            <option selected>Select...</option>
-            <option value="Notebooks">Notebooks</option>
-            <option value="Acessories">Acessories</option>
-            <option value="Gamer">Gamer Experience</option>
-          </select>
-
-          <input
-            class="form-control me-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-danger" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
-    </nav>
-  );
+function SecHeader() {
+	return (
+		<nav class="navbar navbar-dark bg-dark">
+			<div class="container-fluid">
+				<Link to="/">
+					<a class="navbar-brand" href="#">
+						<img src={logo} className="logo_header" />
+					</a>
+				</Link>
+			</div>
+		</nav>
+	);
 }
+export default SecHeader;
