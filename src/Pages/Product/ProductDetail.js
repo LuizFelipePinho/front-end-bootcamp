d82222c6d36c95c58563400e7f0bb39e3656be4b
anacgsantana <anacgsantana@gmail.com>
2022-01-14 11:11:18 -0300

    Update Home and Product Detail

--- src/Pages/Product/ProductDetail.js
+++ src/Pages/Product/ProductDetail.js
@@ -7,18 +7,17 @@ import axios from "axios";
 import styled from "styled-components";
 
 const Img = styled.img`
-  width:17rem;
-  height: 14rem;
- 
+	width: 20rem;
+	height: 17rem;
 `;
 const Model = styled.h1`
-  font-size: 1.5em;
+	font-size: 1.5em;
 `;
 const H3 = styled.h3`
-  font-size: 0.9em;
+	font-size: 0.9em;
 `;
 const Text = styled.p`
-  font-size: 0.9em;
+	font-size: 0.9em;
 `;
 const Description = styled.div`
   display:flex;
@@ -28,58 +27,70 @@ const Description = styled.div`
 `;
 
 function ProductDetail() {
-   const [product, setProduct] = useState([]);
-    
-   const getProductById = async () => {
-    await axios.get(`/product/find/${id}`)
-    .then(response => {
-        setProduct(response.data)
-        
-    })
+	const [product, setProduct] = useState([]);
 
-   }
+	const getProductById = async () => {
+		await axios.get(`/product/find/${id}`).then((response) => {
+			setProduct(response.data);
+		});
+	};
 
-   const { id } = useParams();
+	const { id } = useParams();
 
-   useEffect(() => {
-     getProductById()
-   },[])
+	useEffect(() => {
+		getProductById();
+	}, []);
 
-   
-  return (
-    <Container>
-      <Row>
-        <Col>
-          <Img src={product.productPhotosHard} alt="imagem do produto" />
-        </Col>
-        <Col>
-          <Model >{product.modelHard}</Model>
-          {/* <H3>Store: {product.Vendedor.name}</H3>  */}
-          
-           <Description>
-            <b>Description : </b>
-            <Text>{product.description}</Text>
-            <b>hardware type :</b>
-            <Text>{product.typeHard}</Text>
-            <b>usage time :</b>
-            <Text>{product.yearuseHard}</Text>
-            <b>Product :</b>
-            <Text>{product.usedHard}</Text>
-          </Description>
-          <H3>$:{product.priceHard}</H3>
-          <Button variant="danger" align="center">
-            <FaRegHeart />
-          </Button>{" "}
-          <Link to={`/cart/}`}>
-            <Button variant="danger" align="center">
-              <FaShoppingCart/>
-            </Button>{" "}
-          </Link>
-          
-        </Col>
-      </Row>
-    </Container>
-  );
+	return (
+		<div className="profile">
+			<h1 align="center">Product Detail</h1>
+			<br />
+			<Row>
+				<Col>
+					<div class="embed-responsive embed-responsive-1by1">
+						<iframe
+							width="400"
+							height="250"
+							src={product.videoHard}
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						></iframe>
+					</div>
+				</Col>
+				<Col>
+					<Img
+						src={product.productPhotosHard}
+						alt="imagem do produto"
+					/>
+				</Col>
+				<Col>
+					<Model>{product.modelHard}</Model>
+					{/* <H3>Store: {product.Vendedor.name}</H3>  */}
+					<Description>
+						<b>Description : </b>
+						<Text>{product.description}</Text>
+						<b>Hardware Type :</b>
+						<Text>{product.typeHard}</Text>
+						<b>Usage Time :</b>
+						<Text>{product.yearuseHard}</Text>
+						<b>Product :</b>
+						<Text>{product.usedHard}</Text>
+					</Description>
+					<H3>$:{product.priceHard}</H3>
+					<Button variant="danger" align="center">
+						<FaRegHeart />
+					</Button>{" "}
+					<Link to={`/cart/}`}>
+						<Button variant="danger" align="center">
+							<FaShoppingCart />
+						</Button>{" "}
+					</Link>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
 export default ProductDetail;
